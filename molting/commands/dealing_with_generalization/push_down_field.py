61daea4ac8df04ed6e11154d82ea016f8f319a39
Marshall Yount <marshally@users.noreply.github.com>
2025-11-24 19:58:57 -0800

    ♻️ Refactor push_down_field: Remove _create_field_assignment_statement() wrapper, use utility directly

--- molting/commands/dealing_with_generalization/push_down_field.py
+++ molting/commands/dealing_with_generalization/push_down_field.py
@@ -224,15 +224,6 @@ class PushDownFieldTransformer(cst.CSTTransformer):
             body=class_node.body.with_changes(body=tuple(new_body_stmts))
         )
 
-    def _create_field_assignment_statement(self) -> cst.SimpleStatementLine:
-        """Create a field assignment statement.
-
-        Returns:
-            Field assignment statement
-        """
-        field_value = self.field_value if self.field_value else cst.Integer("0")
-        return create_field_assignment(self.field_name, field_value)
-
     def _create_init_with_field(self) -> cst.FunctionDef:
         """Create new __init__ method with super call and field assignment.
 
@@ -258,7 +249,8 @@ class PushDownFieldTransformer(cst.CSTTransformer):
         if not isinstance(init_node.body, cst.IndentedBlock):
             return init_node
 
-        field_assignment = self._create_field_assignment_statement()
+        field_value = self.field_value if self.field_value else cst.Integer("0")
+        field_assignment = create_field_assignment(self.field_name, field_value)
         new_stmts = list(init_node.body.body) + [field_assignment]
 
         return init_node.with_changes(body=cst.IndentedBlock(body=new_stmts))
